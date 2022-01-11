db3cb0b4c5e6b488eca4a5717e8c16a6efc5cffd
mikedick <mikedick@umich.edu>
2022-01-10 21:08:25 -0500

    Added bio section and mobile menu option.

--- js/app.js
+++ js/app.js
@@ -5,11 +5,5 @@ const nav_slide = () =>{
     burger.addEventListener('click', ()=>{
         nav.classList.toggle('nav-active');
     });
-    // nav_links.forEach((link,index) =>
-    //     {
-    //         link.style.animation = 'nav_link_fase 0.5s ease forward ${index/7}s';
-    //     });
 }
 nav_slide();
-
-// console.log('Click');
